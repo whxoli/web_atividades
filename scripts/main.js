e32ea1badb3a61a72f5573f41fddaeb3b6f9bbce
Olivia Sampaio Prestes <oliviasampaioprestes@gmail.com>
2024-03-06 20:24:59 -0300

    BT21

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,7 @@
 const myImage = document.querySelector("img");
 
-
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "imagens/firefox-icon.png") {
-    myImage.setAttribute("src", "imagens/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "imagens/firefox-icon.png");
-  }
-};
-
-
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-
 
 function setUserName() {
   const myName = prompt("Por favor digite o seu nome.");
@@ -21,7 +9,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla é legal, ${myName}`;
+    myHeading.textContent = `BT21 É INCRIVEL, ${myName}`;
   }
 }
 
